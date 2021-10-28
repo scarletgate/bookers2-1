59c48648d951e727c671ab674b9f801a910d2f84
scarletgate <xiaoyuedu10@gmail.com>
2021-10-28 05:39:04 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
   devise_for :users
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+   resource :favorites, only: [:create, :destroy]
+ end
   resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
