f96e5b7e44dfec38c6d2d1067b3bfa855a5a3022
scarletgate <xiaoyuedu10@gmail.com>
2021-10-28 08:10:08 +0000

    [Finish]応用課題３

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
    resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
  end
   resources :users, only: [:index, :show, :edit, :update, :destroy]
 end
