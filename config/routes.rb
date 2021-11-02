fce1e290ffb47769c4ace441fbe020b90ff824ab
scarletgate <xiaoyuedu10@gmail.com>
2021-11-02 08:04:16 +0000

    [Add]部分一致の検索機能まで作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'home/about', to: 'homes#about'
+  get '/search', to: 'searchs#search'
   devise_for :users
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
    resource :favorites, only: [:create, :destroy]
